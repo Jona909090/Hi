9ecf7b6414b1f5496b05c679f4513721bbd77402
Jona909090 <jonic.ctba@gmail.com>
2026-07-01 11:01:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 window.onload = function () {
-    alert("Radi JavaScript");
+    setTimeout(function () {
+        document.body.classList.remove("not-loaded");
+    }, 1000);
+
+    setTimeout(function () {
+        const message = document.getElementById("message");
+        if (message) {
+            message.classList.add("show");
+        }
+    }, 7000);
 };
