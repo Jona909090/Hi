a6dfafc7a0cd3357a3261253e9f8366d1641f65c
Jona909090 <jonic.ctba@gmail.com>
2026-07-01 10:21:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 onload = () => {
-  const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
-    clearTimeout(c);
-  }, 1000);
+    const c = setTimeout(() => {
+        document.body.classList.remove("not-loaded");
+        clearTimeout(c);
+    }, 1000);
+
+    // Prikaži tekst nakon što se cvijeće otvori
+    setTimeout(() => {
+        document.getElementById("message").classList.add("show");
+    }, 7000);
 };
