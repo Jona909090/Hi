379ab4e104d0b8f90fb5eecfd9479eb8b686a701
Jona909090 <jonic.ctba@gmail.com>
2026-07-01 11:07:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 window.onload = function () {
+
     setTimeout(function () {
         document.body.classList.remove("not-loaded");
     }, 1000);
 
     setTimeout(function () {
-        const message = document.getElementById("message");
-        if (message) {
-            message.classList.add("show");
-        }
+        document.getElementById("message").classList.add("show");
     }, 7000);
+
 };
